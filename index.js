2434b6b7d1f173bf98b75f02b57c18012616c8f1
antonBogomil <anton.bogomil777@gmail.com>
2019-03-24 20:54:16 +0200

    add user registration with password(hashing) and login

--- index.js
+++ index.js
@@ -11,16 +11,37 @@ app.use(bodyParser.json());
 app.use(cookieParser());
 
 const port = process.env.PORT || 3002;
-
-const {User} = require('./src/models/user');
+const {User} = require('./src/models/User');
 /**
- * Users
+ *   Users
  */
 app.post('/api/users/register', (req, res) => {
-    const users = req.body.users || [];
-    users.forEach((u) => {
-        const user = new User(u);
-        user.save();
+    const user = new User(req.body.user);
+    user.save((err, doc) => {
+        if (err) {
+            return res.json({success: false, err})
+        }
+        res.status(200).json({user: doc.name})
+    });
+});
+
+app.post('/api/login', (req, res) => {
+    const {email, password} = req.body;
+    User.findOne({email: email}, (err, user) => {
+        if (!user) {
+            return res.json({message: 'No user with this email!', success: false})
+        }
+        user.comparePassword(password, (err, isMatch) => {
+            if (!isMatch) {
+                return res.json({success: false, message: 'Wrong password!'})
+            }
+            user.generateToken((err, user) => {
+                if (err) {
+                    return res.status(400).send(err)
+                }
+                res.cookie('auth',user.token);
+            })
+        })
     })
 });
 
--- src/models/user.js
+++ src/models/user.js
@@ -1,4 +1,8 @@
 const mongoose = require('mongoose');
+const bcript = require('bcrypt');
+const jwt = require('jsonwebtoken');
+const SALT_NUM = 10;
+require('dotenv').config();
 
 const userSchema = mongoose.Schema({
     email: {
@@ -9,7 +13,7 @@ const userSchema = mongoose.Schema({
     },
     password: {
         type: String,
-        // required: true,
+        required: true,
         minLength: 5
     },
     name: {
@@ -30,5 +34,39 @@ const userSchema = mongoose.Schema({
     }
 
 });
+userSchema.pre('save', function (next) {
+    if (this.isModified()) {
+        bcript.genSalt(SALT_NUM, (err, salt) => {
+            console.log(this);
+            if (err) {
+                return next(err)
+            }
+            bcript.hash(this.password, salt, (err, hash) => {
+                if (err) {
+                    return next(err)
+                }
+                this.password = hash;
+                next()
+            });
+        })
+    } else next();
+});
+
+userSchema.methods.comparePassword = function (password, callback) {
+    bcript.compare(password, this.password, (err, isMatch) => {
+        if (err) return callback(err);
+        return callback(null, isMatch);
+    });
+};
+userSchema.methods.generateToken = function (callback) {
+    const token  = jwt.sign(this._id.toHexString(),process.env.SECRET);
+    this.token = token;
+    this.save((err,user)=>{
+        if (err){return callback(err)}
+        callback(null,user)
+    })
+};
+
 const User = mongoose.model('User', userSchema);
+
 module.exports = {User};
